8b8e47be9972152647d167f6f48e38e30ecccb28
tenyy <84132257@qq.com>
2022-07-11 19:16:44 +0800

    yy

---
+++ assets/Css选择器 权重.html.a5c67a17.js
@@ -0,0 +1,31 @@
+import{_ as n,a as s}from"./app.dfa7130b.js";const e={},a=s(`<h4 id="css\u9009\u62E9\u5668\u7C7B\u578B\u6743\u91CD" tabindex="-1"><a class="header-anchor" href="#css\u9009\u62E9\u5668\u7C7B\u578B\u6743\u91CD" aria-hidden="true">#</a> Css\u9009\u62E9\u5668\u7C7B\u578B\u6743\u91CD</h4><div class="language-css ext-css line-numbers-mode"><pre class="language-css"><code>\u6743\u91CD\u8BA1\u7B97\u7684\u89C4\u5219
+\u5185\u8054\u6837\u5F0F\uFF0C\u5982\uFF1Astyle+\u201C.....\u201D\u6743\u503C\u4E3A1000
+id\u9009\u62E9\u5668\uFF0C\u5982\uFF1A#content\uFF0C\u6743\u503C\u4E3A0100
+\u7C7B\uFF0C\u4F2A\u7C7B\uFF0C\u5C5E\u6027\u9009\u62E9\u5668\uFF0C\u5982content\uFF0C\u6743\u503C\u4E3A0010
+\u7C7B\u578B\u9009\u62E9\u5668\uFF0C\u4F2A\u7C7B\u9009\u62E9\u5668\uFF0C\u5982div p\uFF0C\u6743\u503C\u4E3A0001
+\u7EE7\u627F\u7684\u6837\u5F0F\u6CA1\u6709\u6743\u503C
+\u6743\u91CD\u76F8\u7B49\u7684\u65F6\u5019\uFF0C\u540E\u9762\u7684\u4F1A\u8986\u76D6\u524D\u9762\u7684
+
+\u5F88\u5173\u952E\uFF1A	
+\u5E26\u6709\u5173\u952E\u5B57\uFF01important \u7684 css \u5C5E\u6027 \u6743\u91CD\u65E0\u7A77\u5927\uFF08\u8FD9\u4E2A\u662F\u5C1A\u65B9\u5B9D\u5251\uFF0C\u90A3\u4E2A\u6562\u6321\u8DEF \u5C31\u780D\u8C01<span class="token punctuation">)</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br></div></div><div class="language-reStructuredText ext-reStructuredText line-numbers-mode"><pre class="language-reStructuredText"><code> \u5EF6\u7533\uFF1A\u4E2D\u6587\u548C\u82F1\u6587\u52A0\u8F7D\u7684\u65F6\u5019\uFF0C\u9AD8\u5EA6\u4E0D\u540C
+
+\u200B         \u6BCF\u4E00\u4E2A\u4E2D\u6587\u52A0\u8F7D\u7684\u65F6\u5019\uFF0C\u9AD8\u5EA6\u4E0D\u540C
+
+\u200B         \u6BCF\u4E00\u4E2A\u82F1\u6587\u52A0\u8F7D\u7684\u65F6\u5019\uFF0C\u9AD8\u5EA6\u4E5F\u4E0D\u540C
+
+\u200B         \u5B57\u4F53 \u884C\u9AD8
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br></div></div><div class="language-reStructuredText ext-reStructuredText line-numbers-mode"><pre class="language-reStructuredText"><code> &lt;!-- 
+
+\u200B    ps\uFF1A\u6CE8\u610F\u7F16\u7801\u7EA6\u5B9A
+
+\u200B    \u5728\u4E00\u4E2A\u9875\u9762\u4E2D\uFF0C\u4E00\u4E2A\u7C7B\u540D\u53EF\u4EE5\u88AB\u540C\u65F6\u8D4B\u4E88\u591A\u4E2A\u5143\u7D20\u51FA\u73B0\u591A\u6B21
+
+\u200B    \u5728\u4E00\u4E2A\u9875\u9762\u4E2D\uFF0C\u4E00\u4E2Aid\u540D\u80FD\u4E14\u4EC5\u80FD\u51FA\u73B0\u4E00\u6B21\uFF08\u5E76\u4E0D\u662F\u8BF4\u4E0D\u751F\u6548\uFF0C\u800C\u662F\u7EA6\u5B9A\u53EA\u51FA\u73B0\u4E00\u6B21\uFF09
+
+\u200B    *\u901A\u914D\u7B26\u5C31\u662F\u4E00\u4E2A\u7B26\u53F7\uFF0C\u4E0D\u9700\u8981\u4E5F\u6CA1\u6709\u522B\u7684\u5199\u6CD5\u3002
+
+  --&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br></div></div>`,4);function r(l,p){return a}var i=n(e,[["render",r]]);export{i as default};
